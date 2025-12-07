0e873faaa59fbc17531a1cf8c5073c34373c9c92
Augustin Peyrard <augustin.peyrard@gmail.com>
2025-12-06 23:02:54 -0800

    feat(solution): day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -13,8 +13,27 @@ pub fn part_one(input: &str) -> Option<u64> {
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut grid = Grid::parse_input(input);
+
+    let mut removed_total = 0;
+    let mut removed_this_round = 1;
+    while removed_this_round > 0 {
+        let coords_to_remove: Vec<(usize, usize)> = grid
+            .find(b'@')
+            .filter(|&(x, y)| grid.adjacent_element(x, y, b'@').count() < 4)
+            .collect();
+
+        removed_this_round = coords_to_remove.len();
+
+        for coord in coords_to_remove {
+            grid.set(coord, b'.');
+        }
+
+        removed_total += removed_this_round;
+    }
+
+    Some(removed_total as u64)
 }
 
 #[cfg(test)]
@@ -30,6 +49,6 @@ mod tests {
     #[test]
     fn test_part_two_example_1() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(43));
     }
 }
