e9a3a73db69b6739a774309c643368ca344772fa
Augustin Peyrard <augustin.peyrard@gmail.com>
2026-01-03 23:47:39 -0800

    feat(solution): day 11 part 2
    a bit of recursive DFS with memo, and still using neovim...
    Starting to build a bit of muscle memory... not sure where all of
    that is going, but this might be promising...

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -25,8 +25,54 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(paths)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let devices: HashMap<&str, Vec<&str>> = input
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| line.split_once(':').unwrap())
+        .map(|(device, raw_output)| {
+            (
+                device,
+                raw_output.trim().split_whitespace().collect::<Vec<_>>(),
+            )
+        })
+        .collect();
+
+    let mut cache: HashMap<(&str, bool, bool), u64> = HashMap::new();
+    let count = count_path("svr", false, false, &devices, &mut cache);
+
+    Some(count)
+}
+
+fn count_path<'a>(
+    device: &'a str,
+    dac_seen: bool,
+    fft_seen: bool,
+    devices: &HashMap<&'a str, Vec<&'a str>>,
+    cache: &mut HashMap<(&'a str, bool, bool), u64>,
+) -> u64 {
+    if let Some(&cached_count) = cache.get(&(device, dac_seen, fft_seen)) {
+        return cached_count;
+    }
+    if device == "out" {
+        if dac_seen && fft_seen {
+            return 1;
+        }
+        return 0;
+    }
+
+    let new_dac_seen = dac_seen || device == "dac";
+    let new_fft_seen = fft_seen || device == "fft";
+    let mut count = 0;
+    if let Some(next_devices) = devices.get(device) {
+        for next_device in next_devices {
+            count += count_path(next_device, new_dac_seen, new_fft_seen, devices, cache);
+        }
+    }
+
+    cache.insert((device, new_dac_seen, new_fft_seen), count);
+
+    count
 }
 
 #[cfg(test)]
@@ -35,13 +81,17 @@ mod tests {
 
     #[test]
     fn test_part_one_example_1() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(5));
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_part_two_example_2() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(2));
     }
 }
